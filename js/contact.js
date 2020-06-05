5b0abd010bb05382c827c404743926c66fbd9465
David Huertas <david.huertasf@hotmail.com>
2020-06-05 17:28:26 -0500

    :+1:

--- js/contact.js
+++ js/contact.js
@@ -1,27 +1,12 @@
-var array1 = ["Dinero",
-    "Salarios Pendientes",
-    "Prestaciones Laborales",
-    "Acciones",
-    "Seguros ",
-    "Joyas",
-    "Electrodomésticos",
-    "Vehículos",
-   "Edificaciones ",
-   "Terrenos",
-    "Animales",
-    "Textiles ",
-   " Creaciones de Sofware",
-    "Aplicaciones ",
-   "Creaciones Artisitcas",
-   "Creaciones musicales",
-  "Patentes",
-   "Marcas",
-  "Franquicias",
-   "Empresas"
+var array1 = [
+    "Testamento",
+    "Herencia entre vivos",
+    "Sucesión herencial notarial",
+    "La pertenencia"
 ];
 
 
 
 array1.forEach(element => $(".list-belongings").append( 
-    "<div  class='col-sm-5 col-md-5'><p style='     white-space: nowrap; margin-left:5%;'><input type='checkbox' id='"+element+"' /><label style='margin-left:2%;' for='test1'>"+element+"</label></p></div>" ));
+    "<div  class='col-sm-8 col-md-8'><p style='     white-space: nowrap; '><input type='checkbox' id='"+element+"' /><label style='margin-left:2%;' for='test1'>"+element+"</label></p></div>" ));
 
